df7dd3f6f19da783421cb0f248fea05328d2d45d
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2017-07-04 01:22:16 +0200

    remove remaining doc blocks

--- src/Aggregate/AggregateRootInterface.php
+++ src/Aggregate/AggregateRootInterface.php
@@ -4,29 +4,13 @@ namespace Daikon\Cqrs\Aggregate;
 
 interface AggregateRootInterface
 {
-    /**
-     * @param DomainEventSequence $history
-     * @return AggregateRootInterface
-     */
     public static function reconstituteFromHistory(DomainEventSequence $history): AggregateRootInterface;
 
-    /**
-     * @return AggregateIdInterface
-     */
     public function getIdentifier(): AggregateIdInterface;
 
-    /**
-     * @return AggregateRevision
-     */
     public function getRevision(): AggregateRevision;
 
-    /**
-     * @return DomainEventSequence
-     */
     public function getTrackedEvents(): DomainEventSequence;
 
-    /**
-     * @return AggregateRootInterface
-     */
     public function markClean(): AggregateRootInterface;
 }
